bd928371a2bd04784440bf5378738cdc444cc2ba
Harshitpant12 <harshpant778@gmail.com>
2026-04-13 12:23:33 +0530

    implement login controller logic, provide accesstoken and refreshtoken to user

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -28,4 +28,39 @@ const register = asyncHandler(async function(req, res){
     
 })
 
-export {register}
+const login = asyncHandler(async function(req, res){
+    const {email, password} = req.body
+    
+    const user = await User.findOne({email})
+
+    if(!user) {
+        throw new ApiError(404, "User not found") // or we can use invalid credentials
+    }
+
+    // compare password
+    const isPasswordCorrect = await bcrypt.compare(password, user.password)
+
+    if(!isPasswordCorrect) throw new ApiError(400, "Invalid credentials")
+    
+    const accessToken = user.generateAccessToken()
+    const refreshToken = user.generateRefreshToken()
+
+    user.refreshToken = refreshToken
+    await user.save()
+
+    res.cookie("refreshToken", refreshToken, {
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
+        sameSite: "strict",
+        secure: process.env.NODE_ENV === 'production',
+        httpOnly: true
+    })
+
+    const {password: _, ...userWithoutPassword} = user.toObject()
+
+    res.status(200).json(new ApiResponse(200, {
+        accessToken: accessToken,
+        userWithoutPassword,
+    }, "Logged in successfully"))
+})
+
+export {register, login}
