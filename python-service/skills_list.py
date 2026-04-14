20bc7cbbacc1dfbe30c6d46c7bb9af837ceedfb2
Harshitpant12 <harshpant778@gmail.com>
2026-04-14 06:05:52 +0530

    add list of skills

---
+++ python-service/skills_list.py
@@ -0,0 +1,42 @@
+SKILLS = [
+    # Languages
+    "javascript", "python", "java", "c++", "c", "typescript",
+    "ruby", "kotlin", "swift", "go", "rust", "php", "scala",
+
+    # Frontend
+    "react", "next.js", "vue", "angular", "svelte",
+    "html", "css", "tailwind", "bootstrap", "sass",
+    "redux", "zustand", "context api", "webpack", "vite",
+
+    # Backend
+    "node.js", "express", "django", "flask", "fastapi",
+    "spring boot", "laravel", "ruby on rails",
+
+    # Mobile
+    "flutter", "react native", "android", "ios",
+
+    # Databases
+    "mongodb", "mysql", "postgresql", "sql", "sqlite",
+    "redis", "elasticsearch", "firebase", "supabase",
+
+    # DevOps & Tools
+    "git", "github", "docker", "kubernetes", "aws",
+    "azure", "gcp", "vercel", "render", "linux",
+    "ci/cd", "jenkins", "nginx", "version control"
+
+    # APIs & Protocols
+    "rest api", "graphql", "websockets", "grpc", 'restful apis',
+
+    # AI/ML
+    "machine learning", "deep learning", "nlp",
+    "tensorflow", "pytorch", "scikit-learn", "opencv",
+    "langchain", "computer vision", "ml", "dl",
+
+    # Security & Auth
+    "jwt", "oauth", "oauth2", "bcrypt", "crypto",
+
+    # Concepts
+    "microservices", "system design", "oop", "oops",
+    "computer networks", "operating system", "dsa",
+    "data structures", "algorithms", "agile", "scrum",
+]
