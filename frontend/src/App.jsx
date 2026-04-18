becf41d13f20dd607b8b2fee9c2d96cf78214f06
Harshitpant12 <harshpant778@gmail.com>
2026-04-18 14:55:08 +0530

    set up all routes within authcontextprovider, create separate spaces for public and protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,27 @@ import Home from './pages/Home'
 import { Route, Routes } from 'react-router-dom'
 import Register from './pages/Register'
 import Login from './pages/Login'
+import { AuthContextProvider } from './context/AuthContext'
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
 
   return (
     <>
-        <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/register' element={<Register/>} />
-          <Route path='/login' element={<Login/>} />
-        </Routes>
+        <AuthContextProvider>
+          <Routes>
+            {/* Public Routes */}
+            <Route path='/' element={<Home/>} />
+            <Route path='/register' element={<Register/>} />
+            <Route path='/login' element={<Login/>} />
+
+            {/* Protected Routes */}
+            <Route element={<ProtectedRoute/> } >
+              {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+              {/* all protected route will be added here */}
+            </Route>
+          </Routes>
+        </AuthContextProvider>
     </>
   )
 }
