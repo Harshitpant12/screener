df3809641d9f8f0ac48a57de1f897ff786ac8571
Harshitpant12 <harshpant778@gmail.com>
2026-04-22 19:03:56 +0530

    add frontend url for cors in backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5000
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://skillsync-official.vercel.app',
     credentials: true
 }))
 
